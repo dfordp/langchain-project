ee7efc49264885e9ef40979ede3a8f783a5b9c6d
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-10-21 00:45:02 +0530

    progress 1

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 
+import prismadb from "@/lib/prismadb";
 import { Categories } from "@/components/categories"
 import { Companions } from "@/components/companions"
 import { SearchInput } from "@/components/search-input"
@@ -15,32 +16,32 @@ interface RootPageProps {
 const RootPage = async ({
   searchParams
 }: RootPageProps) => {
-  // const data = await prismadb.companion.findMany({
-  //   where: {
-  //     categoryId: searchParams.categoryId,
-  //     name: {
-  //       search: searchParams.name,
-  //     },
-  //   },
-  //   orderBy: {
-  //     createdAt: "desc"
-  //   },
-  //   include: {
-  //     _count: {
-  //       select: {
-  //         messages: true,
-  //       }
-  //     }
-  //   },
-  // });
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        }
+      }
+    },
+  });
 
-  // const categories = await prismadb.category.findMany();
+  const categories = await prismadb.category.findMany();
 
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
-      {/* <Categories data={categories} /> */}
-      {/* <Companions data={data} /> */}
+      <Categories data={categories} />
+      <Companions data={data} />
     </div>
   )
 }
