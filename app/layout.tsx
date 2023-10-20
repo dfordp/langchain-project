85844af5e2f60999590d03fb538d347306010fc4
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-10-20 23:49:28 +0530

    progress update 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { cn } from '@/lib/utils';
-// import { ThemeProvider } from '@/components/theme-provider'
-// import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster';
 // import { ProModal } from '@/components/pro-modal';
 
 import './globals.css'
@@ -25,11 +25,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn("bg-secondary", inter.className)}>
-          {/* <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-            <ProModal /> */}
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            {/* <ProModal /> */}
             {children}
-            {/* <Toaster />
-          </ThemeProvider> */}
+            <Toaster />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
